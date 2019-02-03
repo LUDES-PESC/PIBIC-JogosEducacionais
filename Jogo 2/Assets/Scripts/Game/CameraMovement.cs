417e924963d7566c19c6b49369320898fd630fb2
Pedro Nascimento <pedrovitor.marques@gmail.com>
2019-02-03 13:54:35 -0200

    Movimento da Camera

---
+++ Jogo 2/Assets/Scripts/Game/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class CameraMovement : MonoBehaviour
+{
+    public Vector3 lastMousePosition;
+    public Vector3 currentMousePosition;
+    public Vector3 delta;
+
+    private void Update()
+    {
+        DragCamera();
+    }
+    private void DragCamera()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePosition = Input.mousePosition;
+            return;
+        }if (Input.GetMouseButton(0))
+        {
+            currentMousePosition = Input.mousePosition;
+            delta = Camera.main.ScreenToWorldPoint(currentMousePosition) - Camera.main.ScreenToWorldPoint(lastMousePosition);
+            delta.z = 0;
+            transform.position -= delta;
+        }if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+        lastMousePosition = currentMousePosition;
+    }
+}
